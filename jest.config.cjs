36a8dc8b539d89bbd33702326166d28d65128874
ByungJoon Lee <imjuni@gmail.com>
2022-07-04 15:03:36 +0900

    Change function interface
    * executeWithoutEither > execute
    * createWithoutEither > create
    * create > createWithEither
    * execute > executeWithEither
    * version bumps ctix@1.1.4
    * apply dts-bundle-generator
    * version bumps: 0.18.0 > 1.0.0

---
+++ jest.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/*.(ts|tsx)', '**/__test__/*.(ts|tsx)'],
+  setupFilesAfterEnv: ['./.config/jest.setup.cjs'],
+};
