b1283dda589f810b02825bb81007185e67f9d59b
ByungJoon Lee <imjuni@gmail.com>
2022-09-05 19:46:04 +0900

    Add new type utility TJinPass, TJinFail

--- src/interfaces/TPassJinEitherFrame.ts
+++ src/interfaces/TPassJinEitherFrame.ts
@@ -1,9 +1,12 @@
 import type { JinEitherFrame } from '@frames/JinEitherFrame';
 import type { IDebugInfo } from '@interfaces/IDebugInfo';
 import type { AxiosResponse } from 'axios';
+import { IPass } from 'my-only-either';
 
 export type TPassJinEitherFrame<T> = AxiosResponse<T> & {
   $progress: 'pass';
   $debug: IDebugInfo;
   $frame: JinEitherFrame<T, any>;
 };
+
+export type TJinPass<T> = IPass<TPassJinEitherFrame<T>>;
