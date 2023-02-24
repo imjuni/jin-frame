5a404c5bad1b2079ca6b34db20d198c1ffad8949
ByungJoon Lee <imjuni@gmail.com>
2023-02-24 11:01:29 +0900

    Enhance duration
    * duration range enhance: milliseconds ~ days

--- src/tools/getDuration.ts
+++ src/tools/getDuration.ts
@@ -1,17 +1,18 @@
 /* eslint-disable-next-line import/no-extraneous-dependencies, import/no-duplicates */
 import intervalToDuration from 'date-fns/intervalToDuration';
 
+/**
+ * getDuration only calculate milliseconds ~ days
+ */
 export function getDuration(start: Date, end: Date) {
   try {
-    const durationSeconds = intervalToDuration({ start, end }).seconds!;
+    const ms = end.getMilliseconds() - start.getMilliseconds();
+    const seconds = intervalToDuration({ start, end }).seconds!;
+    const minutes = intervalToDuration({ start, end }).minutes!;
+    const hours = intervalToDuration({ start, end }).hours!;
+    const days = intervalToDuration({ start, end }).days!;
 
-    if (durationSeconds === 0) {
-      return end.getMilliseconds() - start.getMilliseconds();
-    }
-
-    const duration = durationSeconds * 1000;
-
-    return duration;
+    return ms + seconds * 1000 + minutes * 1000 * 60 + hours * 1000 * 60 * 60 + days * 1000 * 60 * 60 * 24;
   } catch {
     return -1;
   }
