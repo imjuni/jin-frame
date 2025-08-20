5ffb021ce3747593e38b6bfc456418d678710a51
ByungJoon Lee <imjuni@gmail.com>
2025-08-21 01:01:28 +0900

    chore(docs): enhance README.md

--- .configs/esbuild.mjs
+++ .configs/esbuild.mjs
@@ -1,31 +1,31 @@
-import * as esbuild from 'esbuild'
+import * as esbuild from 'esbuild';
 
 if (process.env.FORMAT !== 'cjs' && process.env.FORMAT !== 'esm') {
   console.log(`support "cjs" or "esm"`);
   console.log(`eg. FORMAT=cjs node esbuild.mjs`);
 
-  process.exit(1)
+  process.exit(1);
 }
 
-console.log(`esbuild: ${process.env.FORMAT}`)
+console.log(`esbuild: ${process.env.FORMAT}`);
 
 await esbuild.build({
   entryPoints: ['src/index.ts'],
   bundle: true,
   sourcemap: true,
-  // minify: true,
+  minify: true,
   outfile: process.env.FORMAT === 'cjs' ? 'dist/cjs/index.cjs' : 'dist/esm/index.mjs',
   format: process.env.FORMAT,
   external: [
-    "axios",
-    "fast-safe-stringify",
-    "form-data",
-    "http-status-codes",
-    "merge",
-    "my-easy-fp",
-    "my-only-either",
-    "path-to-regexp",
-    "reflect-metadata",
-    "type-fest",
+    'axios',
+    'fast-safe-stringify',
+    'form-data',
+    'http-status-codes',
+    'merge',
+    'my-easy-fp',
+    'my-only-either',
+    'path-to-regexp',
+    'reflect-metadata',
+    'type-fest',
   ],
-})
+});
